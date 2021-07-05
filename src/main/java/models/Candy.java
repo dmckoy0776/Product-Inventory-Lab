4efab00f1ba3353fedf187e730e830e19e64e3d0
Dee <dee@zipcodes-MacBook-Pro-2.local>
2021-07-05 14:56:38 -0400

    added Ice cream product details

--- src/main/java/models/Candy.java
+++ src/main/java/models/Candy.java
@@ -4,6 +4,8 @@ public class Candy {
     private int id;
     private String brand;
     private String name;
+    private String type;
+    private String size;
     private int qty;
-    private double price;
+    private float price;
 }
--- src/main/java/models/IceCream.java
+++ src/main/java/models/IceCream.java
@@ -1,4 +1,11 @@
 package models;
 
 public class IceCream {
+    private int id;
+    private String brand;
+    private String flavor;
+    private String dairyType;
+    private float price;
+    private int qty;
+
 }

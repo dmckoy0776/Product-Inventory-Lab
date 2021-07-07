4dcec22d51d42f5d2dec18aae549f3509cd840e3
Dee <dee@zipcodes-MacBook-Pro-2.local>
2021-07-07 17:11:36 -0400

    added Candy Service create method and test

--- src/main/java/services/CandyService.java
+++ src/main/java/services/CandyService.java
@@ -1,4 +1,20 @@
 package services;
 
+import models.Candy;
+
+import java.util.ArrayList;
+
 public class CandyService {
+        private static int nextId = 1;
+        private static ArrayList<Candy> inventory = new ArrayList<>();
+
+
+        public static Candy create(String brand, String name, String type, String size, int qty, float price) {
+
+                Candy createdCandy = new Candy(nextId++, brand, name, type, size, qty, price);
+
+                inventory.add(createdCandy);
+
+                return createdCandy;
+        }
 }
--- src/test/java/services/CandyServiceTest.java
+++ src/test/java/services/CandyServiceTest.java
@@ -1,4 +1,44 @@
 package services;
 
+import models.Candy;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CandyServiceTest {
+
+    @Test
+    public void createCandyTest(){
+
+        //Given
+        String brand = "Mars";
+        String name = "Snickers";
+        String type = "chocolate";
+        String size = "full-size";
+        int qty = 20;
+        float price = 1.50F;
+
+        CandyService testService = new CandyService();
+        Candy testCandy = CandyService.create(brand, name, type, size, qty, price);
+
+        //When
+        int actualId = testCandy.getId();
+        String actualBrand = testCandy.getBrand();
+        String actualName = testCandy.getName();
+        String actualType = testCandy.getType();
+        String actualSize = testCandy.getSize();
+        int actualQty = testCandy.getQty();
+        float actualPrice = testCandy.getPrice();
+
+        //Then
+        Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        Assertions.assertEquals(name, actualName);
+        Assertions.assertEquals(brand, actualBrand);
+        Assertions.assertEquals(type, actualType);
+        Assertions.assertEquals(size, actualSize);
+        Assertions.assertEquals(qty, actualQty);
+        Assertions.assertEquals(price, actualPrice);
+
+
+    }
+
 }

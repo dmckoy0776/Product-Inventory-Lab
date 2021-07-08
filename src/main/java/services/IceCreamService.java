a37e5ca1860a71d48c935ab803e19041fe32ce93
Dee <dee@zipcodes-MBP-2.fios-router.home>
2021-07-07 21:41:28 -0400

    IceCreamServices and tests

--- src/main/java/services/IceCreamService.java
+++ src/main/java/services/IceCreamService.java
@@ -1,4 +1,20 @@
 package services;
 
+import models.Candy;
+import models.IceCream;
+
+import java.util.ArrayList;
+
 public class IceCreamService {
+    private static int nextId = 1;
+    private static ArrayList<IceCream> inventory = new ArrayList<>();
+
+    public static IceCream create(String brand, String flavor, String dairyType, float price, int qty) {
+
+        IceCream createdIceCream = new IceCream(nextId++, brand, flavor, dairyType, price, qty);
+
+        inventory.add(createdIceCream);
+
+        return createdIceCream;
+    }
 }
--- src/test/java/services/IceCreamServiceTest.java
+++ src/test/java/services/IceCreamServiceTest.java
@@ -1,4 +1,40 @@
 package services;
 
+import models.Candy;
+import models.IceCream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class IceCreamServiceTest {
+
+    @Test
+    public void createIceCreamTest() {
+
+        //Given
+        String brand = "Hershey";
+        String flavor = "Cookies-n-Creme";
+        String dairyType = "dairy";
+        float price = 5.99F;
+        int qty = 23;
+
+        IceCreamService testService = new IceCreamService();
+        IceCream testCream = IceCreamService.create(brand, flavor, dairyType, price, qty);
+
+        //When
+        int actualId = testCream.getId();
+        String actualBrand = testCream.getBrand();
+        String actualFlavor = testCream.getFlavor();
+        String actualType = testCream.getDairyType();
+        int actualQty = testCream.getQty();
+        float actualPrice = testCream.getPrice();
+
+        //Then
+        Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        Assertions.assertEquals(flavor, actualFlavor);
+        Assertions.assertEquals(brand, actualBrand);
+        Assertions.assertEquals(dairyType, actualType);
+        Assertions.assertEquals(qty, actualQty);
+        Assertions.assertEquals(price, actualPrice);
+
+    }
 }
